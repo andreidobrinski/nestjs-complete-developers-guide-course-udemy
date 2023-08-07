3b4fc63e5966393615b34c36ced08ce07848d638
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-07 12:01:17 -0400

    add naive implementation for messages service

---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+
+  constructor() {
+    // service is creating it's own dependencies. Don't do this on real apps
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
