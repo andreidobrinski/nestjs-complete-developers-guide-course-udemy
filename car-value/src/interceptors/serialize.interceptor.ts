06d32bd9185c56f0b74c6fa65f0c74dc4ec3ac3d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-08 19:21:40 -0400

    add type safety to serialize decorator

--- car-value/src/interceptors/serialize.interceptor.ts
+++ car-value/src/interceptors/serialize.interceptor.ts
@@ -6,14 +6,18 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer'; // plainToInstance can also be used
+import { plainToClass } from 'class-transformer';
 
-export function Serialize(dto: any) {
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  constructor(private dto: ClassConstructor) {}
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // run something before a request is handled by the request handler
