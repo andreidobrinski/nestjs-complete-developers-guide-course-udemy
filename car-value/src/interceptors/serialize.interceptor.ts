364c4e63a6cb67edab0bd81c991dafe10f437b4d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-08 19:15:46 -0400

    refactor to use custom decorator for serialization

--- car-value/src/interceptors/serialize.interceptor.ts
+++ car-value/src/interceptors/serialize.interceptor.ts
@@ -6,17 +6,22 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto';
+import { plainToClass } from 'class-transformer'; // plainToInstance can also be used
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // run something before a request is handled by the request handler
 
     return handler.handle().pipe(
       map((data: any) => {
         // run something before the response is sent out
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
