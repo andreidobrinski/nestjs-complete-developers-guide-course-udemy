c75464d388b619da7389f12f8397a4b7f79f0237
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-15 16:45:10 -0400

    add test for signup

--- car-value/test/auth.e2e-spec.ts
+++ car-value/test/auth.e2e-spec.ts
@@ -32,4 +32,22 @@ describe('Authentication system', () => {
         expect(email).toEqual(testEmail);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const testEmail = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: testEmail, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(testEmail);
+  });
 });
